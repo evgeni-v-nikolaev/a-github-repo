6e7625d72d5296626d023fb301f346c01d556083
Evgeni Nikolaev <evgeni.v.nikolaev@gmail.com>
2022-01-09 16:46:33 -0500

    Added a few generic lines

---
+++ 01_jumpstart.R
@@ -0,0 +1,10 @@
+# Load portfoliodown ----
+# 
+library(portfoliodown)
+
+# Set up your new site ----
+# Run new_portfolio_site(), which will create a Data Science Portfolio Website Template
+# 
+# new_portfolio_site()
+
+serve_site()
